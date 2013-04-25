b7ce4a700fbf4c53172ea38e2fd725aff0f6e04f
Leocadio Tiné <leocadiotine@gmail.com>
2013-04-24 23:17:47 -0400

    Adds a "Hello World" that list all files on the current directory.

--- code/project.clj
+++ code/project.clj
@@ -1,3 +1,6 @@
-(defproject app "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+(defproject fred "1.0.0-SNAPSHOT"
+  :description "A tool to resize images to the various screen densities of Android devices."
+  :dependencies[
+  	[org.clojure/clojure "1.2.1"]
+  	[fs "1.1.2"]]
+  :main fred.core)
--- code/src/fred/core.clj
+++ code/src/fred/core.clj
@@ -1,1 +1,6 @@
-(ns fred.core)
+(ns fred.core
+	(:require [fs.core :as fs])
+	(:gen-class ))
+
+(defn -main [& args]
+	(fs/list-dir "."))
