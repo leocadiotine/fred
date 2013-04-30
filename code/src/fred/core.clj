685adbdfd006d95a24bfaca61254735af74df520
Leocadio Tiné <leocadiotine@gmail.com>
2013-04-29 23:17:10 -0400

    Makes clones of the drawable-xhdpi directory. No resizing yet.

--- code/src/fred/core.clj
+++ code/src/fred/core.clj
@@ -3,7 +3,12 @@
             [fred.image-saver :as image-saver]))
 
 
-(defn -main [& args]
-  (let [path (first args)]
-    (image-saver/copy-to-subdir-and-rename
-      (image-grabber/get-images path) path "drawable-xhdpi")))
+(defn -main [path use-ldpi?]
+  (image-saver/copy-to-subdir-and-rename
+    (image-grabber/get-images path) path "drawable-xhdpi")
+
+  (image-saver/clone-dir
+    path "drawable-xhdpi" "drawable-hdpi" "drawable-mdpi")
+
+  (if use-ldpi? 
+    (image-saver/clone-dir path "drawable-xhdpi" "drawable-ldpi")))
--- code/src/fred/image_saver.clj
+++ code/src/fred/image_saver.clj
@@ -18,5 +18,14 @@
   Also, renames all the files to match Android's resource naming requirements:
   all lowercase, replaces '-' and whitespaces with '_' and removes @2x."
   [file-names original-path subfolder-name]
-  (for [filename file-names]
-    (fs/copy+ (str original-path filename) (str original-path subfolder-name "/" (rename-droid filename)))))
+  (doseq [filename file-names]
+    (fs/copy+ (str original-path filename)
+      (str original-path subfolder-name "/"
+        (rename-droid filename)))))
+
+(defn clone-dir
+  "Makes several copies of original-path. root is the root folder and all the other
+  arguments as folders relative to it."
+  [root-folder original-path & new-paths]
+  (doseq [new-path new-paths]
+    (fs/copy-dir (str root-folder original-path) (str root-folder new-path))))
