10f67600b44d108aa1e05c19600f3731b8ed2ecc
Leocadio Tiné <leocadiotine@gmail.com>
2013-05-01 20:30:04 -0400

    Adds the checkbox for drawable-ldpi.

--- code/src/fred/core.clj
+++ code/src/fred/core.clj
@@ -15,9 +15,8 @@
 
 (defn resize
   "Resizes all images on the given path."
-  [path-images]
+  [path-images use-ldpi?]
   (let [path-folders (get-parent-path path-images)]
-    (println path-folders)
     (image-saver/copy-to-dir-and-rename
       (image-grabber/get-images path-images) path-images (str path-folders "drawable-xhdpi/"))
 
@@ -25,39 +24,40 @@
       path-folders "drawable-xhdpi" "drawable-hdpi" "drawable-mdpi")
 
     (image-resizer/resize-dir path-folders "drawable-hdpi" 0.75)
-    (image-resizer/resize-dir path-folders "drawable-mdpi" 0.5))
+    (image-resizer/resize-dir path-folders "drawable-mdpi" 0.5)
 
-  ;; TODO
-  ; (when (= use-ldpi? true)
-  ;   (image-saver/clone-dir path-folders "drawable-xhdpi" "drawable-ldpi")
-  ;   (image-resizer/resize-dir path-folders "drawable-ldpi" 0.25))
+  (when (= use-ldpi? true)
+    (image-saver/clone-dir path-folders "drawable-xhdpi" "drawable-ldpi")
+    (image-resizer/resize-dir path-folders "drawable-ldpi" 0.25)))
 
   (alert "Your images were resized, buddy. Hope I've been useful.\nDo you want to grab a beer someday?"))
 
 (defn handle-file-drop
   "Grabs the dropped folder and start the resizing process."
-  [data]
+  [data use-ldpi?]
   ;; data needs to be one folder.
   ;; TODO: support dropping multiple files.
   ;; TODO: handle exceptions
-  (resize (str (.get data 0) "/")))
+  (resize (str (.get data 0) "/") use-ldpi?))
 
-(defn -main [path use-ldpi?]
+(defn -main []
   (native!)
+  (def cb (checkbox :text "Create \"drawable-ldpi\"? Sure, I can do that!"))
   (invoke-later
     (-> (frame  :title    "Fred"
                 :content  (flow-panel
-                            :size [512 :by 700]
+                            :size [512 :by 730]
                             :items  [(label :icon (clojure.java.io/resource "icon.png")
                                       :drag-enabled? true
                                       :transfer-handler 
                                         (dnd/default-transfer-handler
-                                          :import [dnd/file-list-flavor (fn [{:keys [data]}] (handle-file-drop data))]))
+                                          :import [dnd/file-list-flavor (fn [{:keys [data]}] (handle-file-drop data (value cb)))]))
                                     (text :multi-line? true
                                           :editable? false
                                           :margin 20
                                           :font "BOLD-14"
-                                          :text "Hely, pal, how is it going? I can resize images for you, it's no trouble\nat all! Just drag a folder and drop inside me and I'll gladly do the work.")])
+                                          :text "Hely, pal, how is it going? I can resize images for you, it's no trouble\nat all! Just drag a folder and drop inside me and I'll gladly do the work.")
+                                    cb])
                 :on-close :exit)
       pack!
       show!)))
